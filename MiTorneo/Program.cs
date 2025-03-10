c2210e2fe737bab438552ea286a042e1ca4f7918
Bidkar Aragón Cárdenas <bidkar@cetis108.edu.mx>
2025-03-10 13:21:22 -0700

    Add MiTorneo project with initial setup and tournament creation logic

---
+++ MiTorneo/Program.cs
@@ -0,0 +1,14 @@
+﻿using Cibacopa;
+
+// crear un torneo
+var miTorneo = new Torneo();
+miTorneo.Nombre = "Claudia Reuchón";
+miTorneo.Edición = 2025;
+miTorneo.Inicia = new DateOnly(2025,3,9);
+miTorneo.Finaliza = new DateOnly(2025,6,30);
+
+// todo: crear 4 equipos
+// todo: crear 3 encuentros para cada equipo como 1ra vuelta
+// todo: crear 3 arbritos
+// todo: crear 5 jugadores por cada equipo
+// todo: crear 1 entrenador por cada equipo
